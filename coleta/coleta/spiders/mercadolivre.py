7a14d807c06e05aafdd540d098c4a060e8d558bc
samudtr <samudosreis@gmail.com>
2024-08-14 16:56:47 +0100

    falta o setup do mercadolivre.py

--- coleta/coleta/spiders/mercadolivre.py
+++ coleta/coleta/spiders/mercadolivre.py
@@ -4,7 +4,19 @@ import scrapy
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     allowed_domains = ["lista.mercadolivre.com.br"]
+
+    ## Site que vai fazer o request: fonte original/macro da informação:
     start_urls = ["https://lista.mercadolivre.com.br/carros-usados#D[A:carros%20usados]"]
 
+
+    ## Configurações do parse ficam abaixo: 
     def parse(self, response):
-        pass
+      
+     products = response.css('div.ui-search-result__content') 
+    
+     for el in products:
+
+      yield { 
+          'title': el.css('div.ui-search-item__group.ui-search-item__group--title').css('h2.ui-search-item__title::text').get() 
+      }
+        
